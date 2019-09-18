bf956a8a64609fa3dda3d9e746c0c9944ba2a0cc
Humberto <humberto@call-em-all.com>
2019-09-18 18:26:32 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./App.css";
+
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  appBar: {
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    paddingLeft: theme.spacing(2),
+    marginBottom: theme.spacing(8),
+  },
+  drawerAppBar: {
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    paddingLeft: theme.spacing(2),
+  },
+  button: {
+    marginLeft: theme.spacing(1),
+  },
+  drawerContainer: {
+    width: '50%'
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Typography variant="h6" className={classes.title}>
+          Recruit-Em-All
+        </Typography>
+      </AppBar>
+      <Button
+        onClick={toggleOpen}
+        className={classes.button}
+        variant="contained"
+        color="primary"
+      >
+        Open Call-Em-All
+      </Button>
+      <Drawer
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerContainer
+        }}
+        onClose={toggleOpen}
+        width="50%"
+      >
+         <AppBar position="static" className={classes.drawerAppBar}>
+            <Typography variant="h6" className={classes.title}>
+              Call-Em-All Integration
+            </Typography>
+          </AppBar>
+         <Container style={{height: '100%'}}>         
+          <div style={{ width: 850, display: 'flex', flexGrow: 1, height: '99%', width: '100%' }}>
+            <iframe
+              title="CEA SSO test"
+              width="100%"
+              height="100%"
+              scrolling="no"
+              src="https://staging-app.call-em-all.com/conversations"
+            />
+          </div>
+        </Container>
+      </Drawer>
+    </div>
+  );
+}
+
+export default App;
